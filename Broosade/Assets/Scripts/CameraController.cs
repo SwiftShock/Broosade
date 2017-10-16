031620faf236a65fa16197d17c22a5d846a6d021
SwiftShock <stoth03@villanova.edu>
2017-10-16 07:44:34 -0400

    Added basic jumping, walking, and camera scripts

---
+++ Broosade/Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+    private Vector3 moveTemp;
+    public float speed = 3;
+    public float xDifference;
+    public float yDifference;
+    public float playerDistance = 3;
+
+	void Update () {
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            xDifference = player.transform.position.x - transform.position.x;
+        }
+        else
+        {
+            xDifference = transform.position.x - player.transform.position.x;
+        }
+
+        if (player.transform.position.y > transform.position.y)
+        {
+            yDifference = player.transform.position.y - transform.position.y;
+        }
+        else
+        {
+            yDifference = transform.position.y - player.transform.position.y;
+        }
+
+        if (xDifference >= playerDistance || yDifference >= playerDistance)
+        {
+            moveTemp = player.transform.position;
+            moveTemp.z = -10;
+
+            transform.position = Vector3.MoveTowards(transform.position, moveTemp, speed * Time.deltaTime);
+        }
+        
+	}
+}
---
+++ Broosade/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed;
+    public float jumpHeight;
+    private Rigidbody2D rigi;
+    public bool isGrounded = false;
+    public Transform groundCheck1;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+
+	// Use this for initialization
+	void Start () {
+        rigi = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck1.position, groundCheckRadius, groundLayer);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) { //jump
+            rigi.velocity = new Vector2(rigi.velocity.x,jumpHeight);
+        }
+
+        if (Input.GetKey(KeyCode.D)) //move right
+        {
+            rigi.velocity = new Vector2(movementSpeed, rigi.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A)) //move left
+        {
+            rigi.velocity = new Vector2(-movementSpeed, rigi.velocity.y);
+        }
+    }
+}
